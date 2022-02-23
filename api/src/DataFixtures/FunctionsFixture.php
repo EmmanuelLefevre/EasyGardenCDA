b7e6ec9bef8afd0bbabb618fe7423120fd229a2f
EmmanuelLefevre <waiasiaa@protonmail.com>
2022-02-23 08:53:35 +0100

    Description des fonctions de fixtures

--- api/src/DataFixtures/FunctionsFixture.php
+++ api/src/DataFixtures/FunctionsFixture.php
@@ -1,14 +1,17 @@
 <?php
 
+// Check string surrounded by parenthesis
 function stringWithoutParenthesis($str) {
     return substr($str, ($p = strpos($str, '(')+1), strrpos($str, ')')-$p);
 }
 
+// Check string surrounded by parenthesis and concatenate parenthesis around
 function stringWithParenthesis($str) {
     $pseudo = substr($str, ($p = strpos($str, '(')+1), strrpos($str, ')')-$p);
     return (' ('.$pseudo.')');
 }
 
+// Generate an aleatory float with two decimals in the interval $min/$max
 function random_float ($min,$max) {
     $value = ($min+lcg_value()*(abs($max-$min)));
     return (sprintf("%01.2f",$value)."bars");
