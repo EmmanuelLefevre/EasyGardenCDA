a11c312544fa8c15d1c7bba3843eac6b675f84d0
EmmanuelLefevre <waiasiaa@protonmail.com>
2022-02-26 15:08:18 +0100

    test

--- api/src/DataProvider/UserDataProvider.php
+++ api/src/DataProvider/UserDataProvider.php
@@ -49,7 +49,7 @@ final class UserDataProvider implements DenormalizedIdentifiersAwareItemDataProv
                 return $this->userRepository->findAll();
             }
             elseif ($user && $role==='ROLE_USER') {
-                return $this->userRepository->find($user);
+                return $user;
             }
         }
     }
