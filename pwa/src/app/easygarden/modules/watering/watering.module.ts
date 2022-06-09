659135fe1c16bce5332bb14e6b5034944f11ba1f
EmmanuelLefevre <waiasiaa@protonmail.com>
2022-06-09 18:02:00 +0200

    watering-module FormsModule + declaration EditWateringComponent

--- pwa/src/app/easygarden/modules/watering/watering.module.ts
+++ pwa/src/app/easygarden/modules/watering/watering.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { WateringRoutingModule } from './watering-routing.module';
 import { WateringComponent } from './components/watering/watering.component';
+import { EditWateringComponent } from './components/editWatering/edit-watering/edit-watering.component';
 
 
 @NgModule({
   declarations: [
-    WateringComponent
+    WateringComponent,
+    EditWateringComponent
   ],
   imports: [
     CommonModule,
     WateringRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 
