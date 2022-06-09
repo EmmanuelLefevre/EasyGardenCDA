f4525760772309de8c6dc2fd6fcc8b18e2c0b1cb
EmmanuelLefevre <waiasiaa@protonmail.com>
2022-06-09 18:00:48 +0200

    watering-routing :id EditWateringComponent

--- pwa/src/app/easygarden/modules/watering/watering-routing.module.ts
+++ pwa/src/app/easygarden/modules/watering/watering-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { WateringComponent } from './components/watering/watering.component';
+import { EditWateringComponent } from './components/editWatering/edit-watering/edit-watering.component';
 
 const routes: Routes = [
   { path: '', component: WateringComponent },
-  { path: ':id', component: WateringComponent }
+  { path: ':id', component: EditWateringComponent }
 ];
 
 @NgModule({
