c5035a1745e78ca48181f7441758d5864c73fb02
EmmanuelLefevre <waiasiaa@protonmail.com>
2022-07-05 23:33:53 +0200

    title pool

--- pwa/src/app/easygarden/modules/pool/components/pool/pool.component.ts
+++ pwa/src/app/easygarden/modules/pool/components/pool/pool.component.ts
@@ -14,6 +14,7 @@ import { ConfirmDialogModel, ConfirmDialogComponent } from 'src/app/easygarden/c
 
 export class PoolComponent implements OnInit, OnDestroy {
 
+  title = "Bassin";
   faPowerOff = faPowerOff;
   faPen = faPen;
   faTrash = faTrash;
