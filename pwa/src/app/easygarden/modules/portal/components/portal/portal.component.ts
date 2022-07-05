aaf81a9c234467425ac0c4193179c8f3d25d6ab4
EmmanuelLefevre <waiasiaa@protonmail.com>
2022-07-05 23:34:29 +0200

    title portal

--- pwa/src/app/easygarden/modules/portal/components/portal/portal.component.ts
+++ pwa/src/app/easygarden/modules/portal/components/portal/portal.component.ts
@@ -14,6 +14,7 @@ import { ConfirmDialogModel, ConfirmDialogComponent } from 'src/app/easygarden/c
 
 export class PortalComponent implements OnInit, OnDestroy {
 
+  title = "Portail";
   faPowerOff = faPowerOff;
   faPen = faPen;
   faTrash = faTrash;
