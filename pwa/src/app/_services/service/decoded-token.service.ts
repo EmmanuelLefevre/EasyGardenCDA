ec076a7c5b202a84bc22505f9411b50457f62675
EmmanuelLefevre <waiasiaa@protonmail.com>
2022-12-22 00:10:07 +0100

    add function to format email

--- pwa/src/app/_services/service/decoded-token.service.ts
+++ pwa/src/app/_services/service/decoded-token.service.ts
@@ -18,7 +18,9 @@ export class DecodedTokenService {
       type tokenInfoModel = JwtPayload & { email: string }
       jwtDecode<tokenInfoModel>(token, { header: true })
       const object = JSON.parse(atob(token.split('.')[1]))
-      const email = object['email']
+      const data = object['email']
+      const explode = data.substring(0, data.indexOf('@'))
+      const email = explode.charAt(0).toUpperCase() + explode.slice(1)
       return email
     }
     return throwError(() => ('Token not found'))
