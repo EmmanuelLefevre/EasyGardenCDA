48f3918a7bb739f25b454af336f034d285348c86
EmmanuelLefevre <waiasiaa@protonmail.com>
2022-12-24 16:31:31 +0100

    loader custom

--- pwa/src/app/app.component.ts
+++ pwa/src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ResolveStart, ResolveEnd, NavigationStart, NavigationCancel, NavigationEnd, NavigationError } from '@angular/router';
+
+import { faLeaf } from '@fortawesome/free-solid-svg-icons';
+import { filter, mapTo, merge, Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -6,6 +11,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  // loading = false;
   title = 'Easy Garden';
+
+  // Loader
+  faLeaf = faLeaf;
+  loading!: Observable<boolean>;
+  private showLoaderEvents!: Observable<boolean>;
+  private hideLoaderEvents!: Observable<boolean>;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.showLoaderEvents = this.router.events.pipe(
+      filter((event:any) => event instanceof ResolveStart),
+      mapTo(true)
+    );
+    this.hideLoaderEvents = this.router.events.pipe(
+      filter((event:any) => event instanceof ResolveEnd),
+      mapTo(false)
+    );
+    this.loading = merge(this.hideLoaderEvents, this.showLoaderEvents);
+  }
+
 }
