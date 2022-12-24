cd2d952889e29ac7850b1a969bd4ceafc424e662
EmmanuelLefevre <waiasiaa@protonmail.com>
2022-12-24 16:51:07 +0100

    lightning resolver

--- pwa/src/app/easygarden/modules/lightning/lightning-routing.module.ts
+++ pwa/src/app/easygarden/modules/lightning/lightning-routing.module.ts
@@ -5,8 +5,13 @@ import { LightningComponent } from './components/lightning/lightning.component';
 import { EditLightningComponent } from './components/editLightning/edit-lightning.component';
 import { AddLightningComponent } from './components/addLightning/add-lightning.component';
 
+import { LightningResolver } from './lightning.resolver';
+
 const routes: Routes = [
-  { path: '', component: LightningComponent },
+  { path: '', component: LightningComponent, resolve: {
+      lightning: LightningResolver
+    }
+  },
   { path: 'edit/:id', component: EditLightningComponent },
   { path: 'add', component: AddLightningComponent }
 ];
---
+++ pwa/src/app/easygarden/modules/lightning/lightning.resolver.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Router, Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { catchError, Observable, throwError, delay } from 'rxjs';
+
+import { LightningService } from './lightning.service';
+import { DataUserModel } from 'src/app/_models/userModel';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class LightningResolver implements Resolve<DataUserModel[]> {
+  
+  constructor(private lightningService: LightningService,
+              private router: Router) {}
+
+  resolve(route: ActivatedRouteSnapshot): Observable<DataUserModel[]> {
+    return this.lightningService.getAllLightnings().pipe(
+      delay(1200),
+      catchError(
+        () => {
+          this.router.navigate([""]);
+          return throwError(() => ('Aucune donnée n\'a été trouvée.'))
+        }
+      )
+    );
+  }
+}
