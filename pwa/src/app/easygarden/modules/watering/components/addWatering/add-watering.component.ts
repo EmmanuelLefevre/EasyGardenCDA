4b69ec8af64194f1547cfcf163e70af7ea9e54b6
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-02-16 17:09:37 +0100

    fix

--- pwa/src/app/easygarden/modules/watering/components/addWatering/add-watering.component.ts
+++ pwa/src/app/easygarden/modules/watering/components/addWatering/add-watering.component.ts
@@ -32,7 +32,7 @@ export class AddWateringComponent implements OnInit {
   success = '';
   watering!: IWatering;
   
-  // Snackbar display which garden are owned by user
+  // Snackbar display which gardens are owned by user
   gardens: IGarden[] = [];
   selected = '';
   gardenName = '';
